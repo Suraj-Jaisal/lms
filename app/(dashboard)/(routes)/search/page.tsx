d5350d47a178c3ee16182b33f6b04ac422ea0420
suraj jaisal <surajjaisal445@gmail.com>
2023-12-04 18:19:34 +0530

    course create

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -4,6 +4,9 @@ import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 
 import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { CoursesList } from "@/components/courses-list";
 
 interface SearchPageProps {
   searchParams: {
@@ -25,10 +28,18 @@ const SearchPage = async ({ searchParams }: SearchPageProps) => {
     },
   });
 
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
   return (
     <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
       <div className="p-6 space-y-4">
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
